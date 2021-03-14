85aee5c510cde0ca01ed56d394894faea1ce69e3
peterspetz <peter@workbuster.se>
2021-03-14 20:01:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# google-app-engine-node-deploy-example
+
+Example of basic Node.js app deployment setup on Google App Engine using Github Actions.
+
+Read more in [this blog post](https://tomekkolasa.com/how-to-deploy-node-js-app-to-google-app-engine-using-github-actions).
---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get("/", async (req, res) => {
+  res.json({ status: "OK" });
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
